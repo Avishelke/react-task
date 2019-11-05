4a13c11caf1b52ab05083481df4b6144323a9e9c
avishelke <avishelke059@gmail.com>
2019-11-05 23:25:11 +0530

    Updates

---
+++ precache-manifest.555761a9d7580d52ab59d9f67eb40bef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42dfcde86f1fb43b7697",
+    "url": "/react-task/static/js/runtime~main.593f8690.js"
+  },
+  {
+    "revision": "712b3ca520ac0ec84c54",
+    "url": "/react-task/static/js/main.a39c1d31.chunk.js"
+  },
+  {
+    "revision": "2f698abeb4d62ed17aef",
+    "url": "/react-task/static/js/2.585bb5ad.chunk.js"
+  },
+  {
+    "revision": "712b3ca520ac0ec84c54",
+    "url": "/react-task/static/css/main.19292571.chunk.css"
+  },
+  {
+    "revision": "6fdb41dcb64d265a5512278d383b0800",
+    "url": "/react-task/index.html"
+  }
+];
